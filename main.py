0324fc6278230f3b8c049a7e7f28542f54f1e6bc
RicardoDuranS <139406635+RicardoDuranS@users.noreply.github.com>
2024-08-21 10:30:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
+@app.route('/test')
+def index():
+  return 'Test succes'
+
 if __name__ == '__main__':
   app.run(port=5000)
